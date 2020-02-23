c5b0dcd79086a05693ca508f5ed4afdc0e4a0ba5
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 15:10:18 +0300

    qulifier annotation added and property greeting service annotated to it

--- src/main/java/com/mahdidroid/di/DiApplication.java
+++ src/main/java/com/mahdidroid/di/DiApplication.java
@@ -8,16 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class DiApplication {
 
     public static void main(String[] args)
     {
        ApplicationContext ctx= SpringApplication.run(DiApplication.class, args);
-       MyController controller = (MyController)ctx.getBean(("myController"));
-       controller.hello();
-
+       //MyController controller = (MyController)ctx.getBean(("myController"));
+       //controller.hello();
+        System.out.println(ctx.getBean(MyController.class).hello());
         System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+
         System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 
