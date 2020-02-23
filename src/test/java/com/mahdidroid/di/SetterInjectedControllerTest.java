fdaf1262c38d7196b9faf30dd844064f58c07bd1
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 11:34:06 +0300

    SetterInjectedControllerTest class added

---
+++ src/test/java/com/mahdidroid/di/SetterInjectedControllerTest.java
@@ -0,0 +1,11 @@
+package com.mahdidroid.di;
+
+import com.mahdidroid.di.Controller.SetterInjectedController;
+import org.junit.Before;
+
+public class SetterInjectedControllerTest {
+    private SetterInjectedController setterInjectedController;
+
+    @Before
+
+}
