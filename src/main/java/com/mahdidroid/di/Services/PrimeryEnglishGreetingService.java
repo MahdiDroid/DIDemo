701f90162d49c8906d8a4ed48de736997f5d9762
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 16:27:18 +0300

    PrimaryGreetingService renamed to PrimaryEnglishGreetingService annotaed as Profile(en)

--- src/main/java/com/mahdidroid/di/Services/PrimeryEnglishGreetingService.java
+++ src/main/java/com/mahdidroid/di/Services/PrimeryEnglishGreetingService.java
@@ -2,11 +2,13 @@ package com.mahdidroid.di.Services;
 
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-public class PrimeryGreetingService implements GreetingService {
+@Profile("en")
+public class PrimeryEnglishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
         return "Hallo from the primery greeting service";
