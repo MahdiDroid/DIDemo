e8881dea731947c0dc106aec66e3961e1af1e305
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 15:44:43 +0300

    PrimarySpanishGreetingService annotated as service

--- src/main/java/com/mahdidroid/di/Services/PrimarySpanishGreetingService.java
+++ src/main/java/com/mahdidroid/di/Services/PrimarySpanishGreetingService.java
@@ -1,9 +1,9 @@
 package com.mahdidroid.di.Services;
 
 
+import org.springframework.stereotype.Service;
 
-
-
+@Service
 public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
