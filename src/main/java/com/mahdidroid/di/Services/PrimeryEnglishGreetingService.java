3f0b00ffd1282701830942b380fdd94626f8bd9e
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 17:00:33 +0300

    default profile annotation added to primary english greeting service

--- src/main/java/com/mahdidroid/di/Services/PrimeryEnglishGreetingService.java
+++ src/main/java/com/mahdidroid/di/Services/PrimeryEnglishGreetingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile("en")
+@Profile({"en","default"})
 public class PrimeryEnglishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
