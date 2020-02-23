9123d6a1b9ef91343e39879139173c7f5cfd61b1
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 15:48:11 +0300

    Pramary annotation added to PrimarySpanishGreetingService

--- src/main/java/com/mahdidroid/di/Services/PrimarySpanishGreetingService.java
+++ src/main/java/com/mahdidroid/di/Services/PrimarySpanishGreetingService.java
@@ -1,11 +1,13 @@
 package com.mahdidroid.di.Services;
 
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile("es")
+@Primary
 public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
