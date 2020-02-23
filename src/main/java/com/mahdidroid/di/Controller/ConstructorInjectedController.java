12971a2117cab640579455d9a2c51debfda2487e
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 13:18:00 +0300

    Qualifier annotation added to Consctructor injected and setter injected Controllers

--- src/main/java/com/mahdidroid/di/Controller/ConstructorInjectedController.java
+++ src/main/java/com/mahdidroid/di/Controller/ConstructorInjectedController.java
@@ -2,14 +2,16 @@ package com.mahdidroid.di.Controller;
 
 import com.mahdidroid.di.Services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
-    @Autowired //its optional in constructor injection
+   // @Autowired //its optional in constructor injection
     private GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+
+    public ConstructorInjectedController( @Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/main/java/com/mahdidroid/di/Controller/SetterInjectedController.java
+++ src/main/java/com/mahdidroid/di/Controller/SetterInjectedController.java
@@ -3,14 +3,16 @@ package com.mahdidroid.di.Controller;
 import com.mahdidroid.di.Services.GreetingService;
 import com.mahdidroid.di.Services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
 
-    @Autowired
-    private GreetingService  greetingService;
 
+    private GreetingService  greetingService;
+    @Autowired
+    @Qualifier("getterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
