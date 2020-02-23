9de8e826d7c903f436e412473cb942ec0c76fa23
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 10:53:05 +0300

    GreetingService class  implimented

--- src/main/java/com/mahdidroid/di/Services/GreetingServiceImpl.java
+++ src/main/java/com/mahdidroid/di/Services/GreetingServiceImpl.java
@@ -4,7 +4,11 @@ package com.mahdidroid.di.Services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GreetingServiceImpl {
+public class GreetingServiceImpl implements GreetingService {
 
-
+public static final String Hello_folks = "Hello Folks";
+    @Override
+    public String sayGreeting() {
+        return Hello_folks;
+    }
 }
