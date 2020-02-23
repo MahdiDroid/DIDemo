17234ad2e7e1bdf5fb0399b67c8b671e306a640e
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 13:27:39 +0300

    class type changed to interface type in PropertyInjectedController

--- src/main/java/com/mahdidroid/di/Controller/PropertyInjectedController.java
+++ src/main/java/com/mahdidroid/di/Controller/PropertyInjectedController.java
@@ -1,16 +1,16 @@
 package com.mahdidroid.di.Controller;
 
-import com.mahdidroid.di.Services.GreetingServiceImpl;
+import com.mahdidroid.di.Services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
     @Autowired
-    public GreetingServiceImpl greetingService;
+    public GreetingService propertyGreetingService;
 
     public String sayHello(){
-        return greetingService.sayGreeting();
+        return propertyGreetingService.sayGreeting();
     }
 
 
