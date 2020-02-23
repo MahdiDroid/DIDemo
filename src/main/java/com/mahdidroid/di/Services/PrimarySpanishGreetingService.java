92b969a2fcc966961e8c9c3016ef1fd38ebe87a2
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 15:46:42 +0300

    profile annotation added to PrimarySpanishGreetingService

--- src/main/java/com/mahdidroid/di/Services/PrimarySpanishGreetingService.java
+++ src/main/java/com/mahdidroid/di/Services/PrimarySpanishGreetingService.java
@@ -1,9 +1,11 @@
 package com.mahdidroid.di.Services;
 
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("es")
 public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
