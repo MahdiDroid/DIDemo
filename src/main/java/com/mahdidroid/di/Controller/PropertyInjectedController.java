06aac2861278ca6b1ac7625de43eb42da7e795e6
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 10:57:42 +0300

    greetingService property added to property injected Controller

--- src/main/java/com/mahdidroid/di/Controller/PropertyInjectedController.java
+++ src/main/java/com/mahdidroid/di/Controller/PropertyInjectedController.java
@@ -1,6 +1,14 @@
 package com.mahdidroid.di.Controller;
 
+import com.mahdidroid.di.Services.GreetingServiceImpl;
+
 public class PropertyInjectedController {
+    public GreetingServiceImpl greetingService;
+
+    String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
 
 
 
