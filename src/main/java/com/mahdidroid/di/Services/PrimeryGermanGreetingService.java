d59b8ee08c62a145903a3774df7df2bc4b2ab4ee
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 16:36:17 +0300

    Primary germanGreetingService created

---
+++ src/main/java/com/mahdidroid/di/Services/PrimeryGermanGreetingService.java
@@ -0,0 +1,16 @@
+package com.mahdidroid.di.Services;
+
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("en")
+public class PrimeryGermanGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hallo from the primery greeting service";
+    }
+}
