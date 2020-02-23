8040aa69dbe515de319c0cae177dc05e0dfd0202
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 15:44:14 +0300

    PrimarySpanishGreetingService implements GreetingService

--- src/main/java/com/mahdidroid/di/Services/PrimarySpanishGreetingService.java
+++ src/main/java/com/mahdidroid/di/Services/PrimarySpanishGreetingService.java
@@ -1,4 +1,12 @@
 package com.mahdidroid.di.Services;
 
-public class PrimarySpanishGreetingService {
+
+
+
+
+public class PrimarySpanishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return null;
+    }
 }
