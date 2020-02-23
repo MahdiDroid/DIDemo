ecef3a43f62f4653d0dbcd66792e6df43fdee30b
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 13:03:47 +0300

    getter greeting service class Annotaed as service

--- src/main/java/com/mahdidroid/di/Services/GetterGreetingService.java
+++ src/main/java/com/mahdidroid/di/Services/GetterGreetingService.java
@@ -1,5 +1,8 @@
 package com.mahdidroid.di.Services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GetterGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
