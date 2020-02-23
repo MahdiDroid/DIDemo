08472cabc93328afebc2b1870f67a7518a029a77
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 11:08:38 +0300

    Constructor injected Controller implements with constructor injected

--- src/main/java/com/mahdidroid/di/Controller/ConstructorInjectedController.java
+++ src/main/java/com/mahdidroid/di/Controller/ConstructorInjectedController.java
@@ -1,4 +1,17 @@
 package com.mahdidroid.di.Controller;
 
+import com.mahdidroid.di.Services.GreetingService;
+
 public class ConstructorInjectedController {
+
+    private GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
 }
